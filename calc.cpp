c7fcc3fb81406950c86d345731d3994a92db2e1e
NotMizzel <113567858+NotMizzel@users.noreply.github.com>
2022-12-24 17:18:03 -0600

    Added more source code developed over vaycay

--- calc.cpp
+++ calc.cpp
@@ -1,47 +1,65 @@
 #include <iostream>
 
+#include <type_traits>
+
 using std::cout;
 using std::cin;
 using std::string;
-int main()
-{
-	int timesdone = 0;
-	while(true)
-	{
-		double firstnum;
-		cout << "Enter first number: ";
-		cin >> firstnum;
-		string op;
-		cout << "Enter operator: ";
-		cin >>  op;
-		double secnum;
-		cout << "Enter second number: ";
-		cin >> secnum;
-		if((op)=="*"){
-			cout << firstnum * secnum << "\n";
-		}
-		else if((op)=="/"){
-			cout << firstnum / secnum << "\n";
-		}
-		else if (op == "+") {
-    			if (firstnum == 9 && secnum == 10) {
-       				 timesdone++;
-   		 	}
+int main() {
+int numerror = 0;
+int timesdone = 0;
+while (true) {
+    double firstnum;
+    cout << "Enter first number: ";
+    cin >> firstnum;
 
-	    		if (timesdone == 3) {
-        			cout << 21 << "\n";
-  			     	timesdone = 0;
-   			} else {
-    		    		cout << firstnum + secnum << "\n";
-    			}
+    if (!std::is_floating_point < decltype(firstnum) > ()) {
+        cout << "Error";
+        numerror = 1;
+    }
 
-		}
-		else if((op)=="-"){
-			cout << firstnum - secnum << "\n";
-		}
-		else{
-		cout << "Error\n";
-		}
-	}
+    if (numerror == 0) {
+        string op;
+        cout << "Enter operator: ";
+        cin >> op;
+
+        if (cin.fail()) {
+            cout << "Error\n";
+            cin.clear();
+            cin.ignore(std::numeric_limits < std::streamsize > ::max(), '\n');
+            continue;
+        }
+
+        double secnum;
+        cout << "Enter second number: ";
+        cin >> secnum;
+
+        if (!std::is_floating_point < decltype(secnum) > ()) {
+            numerror = 1;
+            cout << "Error";
+        }
+        if (numerror == 0) {
+            if ((op) == "*") {
+                cout << firstnum * secnum << "\n";
+            } else if ((op) == "/") {
+                cout << firstnum / secnum << "\n";
+            } else if (op == "+") {
+                if (firstnum == 9 && secnum == 10) {
+                    timesdone++;
+                }
+
+                if (timesdone == 3) {
+                    cout << 21 << "\n";
+                    timesdone = 0;
+                } else {
+                    cout << firstnum + secnum << "\n";
+                }
+            } else if ((op) == "-") {
+                cout << firstnum - secnum << "\n";
+            } else {
+                cout << "Error\n";
+            }
+        }
+    }
 }
-
+}
